4f315c23d07673072803dd88943d9d4a44db77d7
Tai <taicorraggio@gmail.com>
2025-06-14 11:19:21 -0400

    Add additional books routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,21 +17,33 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const author = req.params.author;
+    const book = Object.keys(books).forEach(isbn => {
+        const book = books[isbn];
+        if (book.author.toLowerCase() === author.toLowerCase()) {
+            return res.send(book);
+        }
+    })
+    res.status(404).json({ message: "Author not found" });
+ });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const title = req.params.title;
+    const book = Object.keys(books).forEach(isbn => {
+        const book = books[isbn];
+        if (book.title.toLowerCase() === title.toLowerCase()) {
+            return res.send(book);
+        }
+    })
+    res.status(404).json({ message: "Title not found" });
+ });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
